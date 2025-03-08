7fbcd55e1f32fc4762b15d26b6cfeb73a1a1b7a9
S-CHANDRU03 <chandrusounthirarajan@gmail.com>
2025-03-08 19:44:02 +0530

    Fully updated responsive form

--- script.js
+++ script.js
@@ -7,20 +7,84 @@ document.getElementById("registrationForm").addEventListener("submit", function 
     const gender = document.getElementById("gender").value;
     const course = document.getElementById("course").value;
     const email = document.getElementById("email").value;
+
+    var nameRegex = /^[a-zA-Z\s]+$/
+    var ageRegex = /^\d+$/
+    var mailRegex = /^[a-zA-Z0-9]+@gmail\.com/
+
+    var validate = true
+
+    if (nameRegex.test(name) == false)
+    {
+      document.querySelector(".nameError").style.display="inline"
+      validate = false
+      
+    }
+    else {
+    document.querySelector(".nameError").style.display="none"
+    }
+
+    if (ageRegex.test(age) == false)
+    {
+    document.querySelector(".ageError").style.display="inline"
+    validate = false
+    
+    }
+    else {
+    document.querySelector(".ageError").style.display="none"
+    }
+
+    if (gender == '')
+    {
+    document.querySelector(".genderError").style.display="inline"
+    validate = false
+    
+    }
+    else {
+    document.querySelector(".genderError").style.display="none"
+    }
+
+    if (course == '')
+    {
+    document.querySelector(".courseError").style.display="inline"
+    validate = false
+    
+    }
+    else {
+    document.querySelector(".courseError").style.display="none"
+    }
+
+    if (mailRegex.test(email) == false)
+    {
+    document.querySelector(".mailError").style.display="inline"
+    validate = false
+    
+    }
+    else {
+    document.querySelector(".mailError").style.display="none"
+    }
+
+    if (validate == false)
+    {
+        return
+    }
+    else{
+        alert("form submitted successfully")
+    }
   
     // Create a new row in the table
     const tableBody = document.querySelector("#studentTable tbody");
     const newRow = document.createElement("tr");
   
     newRow.innerHTML = `
-      <td>${name}</td>
-      <td>${age}</td>
-      <td>${gender}</td>
-      <td>${course}</td>
-      <td>${email}</td>
-      <td class="actions">
-        <button class="edit" onclick="editRow(this)">Edit</button>
-        <button class="delete" onclick="deleteRow(this)">Delete</button>
+      <td data-label="Name:">${name}</td>
+      <td data-label="Age:">${age}</td>
+      <td data-label="Gender:">${gender}</td>
+      <td data-label="Course:">${course}</td>
+      <td data-label="Email:">${email}</td>
+      <td data-label="Action:">
+      <i id="edit" onclick="editRow(this)" class="fa-solid fa-user-pen"></i>
+      <i id="trash" onclick="deleteRow(this)" class="fa-solid fa-trash"></i>
       </td>
     `;
   
